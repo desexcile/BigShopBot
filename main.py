ef81630b5874386ec566672b67943f690440c471
desexcile <34170928+desexcile@users.noreply.github.com>
2020-09-11 09:51:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def get_info_from_selenium(link):
     print('Cookie 1 = ')
     print(driver.get_cookies())
     driver.delete_cookie("aep_usuc_f")
-    driver.add_cookie({'name': 'aep_usuc_f', 'path': '/', 'sameSite': 'Lax', 'secure': True,
+    driver.add_cookie({'name': 'aep_usuc_f', 'path': '/', 'sameSite': 'Strict', 'secure': True,
                        'value': 'site=rus&c_tp=RUB&region=RU&b_locale=ru_RU'})
     usd_price = driver.find_element_by_class_name('product-price-value').text.split('$')[-1]
     print('Cookie 2 = ')
